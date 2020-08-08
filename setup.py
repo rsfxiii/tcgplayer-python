c2ecf20c27831aed0bd3aaa4008fb0343433f7ed
Ryan <ryanshawferguson@gmail.com>
2020-08-07 21:00:34 -0400

    Fix format string quotes

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import config, setup
 import os
 
 if __name__ == '__main__':
-    confpath = f'{os.getcwd()}/setup.cfg'
+    confpath = f"{os.getcwd()}/setup.cfg"
     config.read_configuration(confpath)
     setup()
