d0f07768c82a10d9c8a0a49436943b361bf5de07
Ryan <ryanshawferguson@gmail.com>
2020-08-20 09:52:33 -0400

    Add TestCase with a couple placeholder tests for Client

--- tcgplayer/tests/test_client.py
+++ tcgplayer/tests/test_client.py
@@ -2,7 +2,27 @@ import unittest
 
 from src.client import Client
 
-# TODO: Rewrite to test the outcome of Client, rather than its state as an instance \
-# because the tests don't need to be coupled to the implementation, but testing the results of it
 
+class ClientTestCase(unittest.TestCase):
+    def setUp(self):
+        self.credentials = {
+            'PUB_KEY': 'test-pub-key',
+            'PRIV_KEY': 'test-priv-key'
+        }
 
+    def test_build_access_payload(self):
+        expected = {
+            'grant_type': 'client_credentials',
+            'client_id': 'test-pub-key',
+            'client_secret': 'test-priv-key'
+        }
+        self.assertEquals(Client.build_access_payload(self.credentials['PUB_KEY'],
+                                                      self.credentials['PRIV_KEY'], expected))
+
+    # TODO: How can we test Client.refresh_access_token()?
+    def test_refresh_access_token(self):
+        pass
+
+    # TODO: How can we test Client.handle_request()?
+    def test_handle_request(self):
+        pass
