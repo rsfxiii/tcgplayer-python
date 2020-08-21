aca0ef7efc202a8d12c8e0b67b708493bcb2c9ec
Ryan <ryanshawferguson@gmail.com>
2020-08-20 21:07:34 -0400

    Move config to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,28 @@
-from setuptools import config, setup
+from setuptools import setup, find_packages
 
-import os
 
 if __name__ == '__main__':
-    confpath = f"{os.getcwd()}/setup.cfg"
-    config.read_configuration(confpath)
-    setup()
+    setup(
+        name='tcgplayer',
+        description='Python 3 library for accessing TCGPlayer APIs',
+        version='0.8',
+        author='rsfxiii',
+        author_email='ryan@raijinn.xyz',
+        url='https://github.com/rsfxiii/TCGPLayer',
+        license='GNU General Public License v3',
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Games/Entertainment',
+        ],
+        namespace_packages=['tcgplayer'],
+        packages=find_packages(include=['tcgplayer', 'tcgplayer.*']),
+        install_requires=[
+            'requests',
+            'python-dotenv',
+            'coverage'
+        ],
+        python_requires='>=3.7',
+    )
